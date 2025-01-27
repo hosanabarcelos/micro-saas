ba50fb4d2f0fa1a48ff4433b0ed2d938c491629f
Hosana <hosanabarceloscandido@gmail.com>
2025-01-27 13:33:30 -0300

    fix: corrige estrutura de pastas de acordo com regras do next

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -25,4 +25,5 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
