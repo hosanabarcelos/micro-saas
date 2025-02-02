52d8fc0f06054591c2ae9e224e21b678d84e6229
Hosana <hosanabarceloscandido@gmail.com>
2025-02-02 15:44:24 -0300

    feat: add lógica para card de projeto dinâmico

--- app/components/commons/project-card.tsx
+++ app/components/commons/project-card.tsx
@@ -1,24 +1,55 @@
-export default function ProjectCard() {
-  return (
-    <div className="w-[340px] h-[132px] flex gap-5 bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
-      <div className="size-24 rounded-md overflow-hidden flex-shrink-0">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4DGVSPbykIyP9h5mMfwhXeeb6LD0Nz4VxjQ&s.png"
-          alt="Projeto"
-          className="w-full h-full object-cover"
-        />
+"use client";
+
+import { ProjectData } from "@/app/server/get-profile-data";
+import Link from "next/link";
+
+export default function ProjectCard({
+  project,
+  isOwner,
+  img,
+}: {
+  project?: ProjectData;
+  isOwner: boolean;
+  img: string;
+}) {
+  if (!project) {
+    return (
+      <div className="w-[340px] h-[132px] flex items-center justify-center bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
+        <span className="text-content-body text-sm">Projeto não encontrado</span>
       </div>
-      <div className="flex flex-col gap-2">
-        <span className="uppercase text-xs font-bold text-accent-green">
-          10 cliques
-        </span>
-        <div className="flex flex-col">
-          <span className="text-white font-bold">Projeto 1</span>
-          <span className="text-content-body text-sm">
-            Descrição super detalhada do que o projeto faz.
-          </span>
+    );
+  }
+
+  const projectUrl = project.projectUrl || "#"; // fallback URL
+  const formattedUrl = projectUrl.startsWith("http")
+    ? projectUrl
+    : `https://${projectUrl}`;
+
+  function handleClick() {
+    console.log("clicked"); // TODO: analytics
+  }
+
+  return (
+    <Link href={formattedUrl} target="_blank" onClick={handleClick}>
+      <div className="w-[340px] h-[132px] flex gap-5 bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
+        <div className="size-24 rounded-md overflow-hidden flex-shrink-0">
+          <img src={img} alt="Projeto" className="w-full h-full object-cover" />
+        </div>
+        <div className="flex flex-col gap-2">
+          {isOwner && (
+            <span className="uppercase text-xs font-bold text-accent-green">
+              {project.totalVisits || 0} cliques
+            </span>
+          )}
+
+          <div className="flex flex-col">
+            <span className="text-white font-bold">{project.projectName}</span>
+            <span className="text-content-body text-sm">
+              {project.projectDescription}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
