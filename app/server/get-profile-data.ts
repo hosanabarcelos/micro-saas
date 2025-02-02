e88fc2ed1666cc8b690847f441ad5508a771beea
Hosana <hosanabarceloscandido@gmail.com>
2025-02-02 15:46:07 -0300

    feat: add dados de usuário

--- app/server/get-profile-data.ts
+++ app/server/get-profile-data.ts
@@ -6,11 +6,32 @@ export type ProfileData = {
   userId: string;
   totalVisits: number;
   createdAt: number;
-  // adicionar mais depois - todo
+  // adicionar mais depois - TODO
+};
+
+export type ProjectData = {
+  id: string;
+  userId: string;
+  projectName: string;
+  projectDescription: string;
+  projectUrl: string;
+  imagePath: string;
+  createdAt: string;
+  totalVisits?: number;
 };
 
 export async function getProfileData(profileId: string) {
   const snapshot = await db.collection("profiles").doc(profileId).get();
 
   return snapshot.data() as ProfileData;
+}
+
+export async function getProfileProjects(profileId: string) {
+  const snapshot = await db
+    .collection("projects")
+    .doc(profileId)
+    .collection("projects")
+    .get();
+
+  return snapshot.docs.map((doc) => doc.data() as ProjectData);
 }
