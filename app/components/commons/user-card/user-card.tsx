99c0b16e0e13b5ca7c53ca4b98c25c7c9ad26eeb
Hosana <hosanabarceloscandido@gmail.com>
2025-02-07 01:30:55 -0300

    fix: corrige bug de erro ao acessar a landing page

--- app/components/commons/user-card/user-card.tsx
+++ app/components/commons/user-card/user-card.tsx
@@ -1,4 +1,4 @@
-import { Github, Instagram, Linkedin, Twitter } from "lucide-react";
+import { Github, Instagram, Linkedin, Twitter, Plus } from "lucide-react";
 import EditSocialLinks from "./edit-social-links";
 import Button from "../../ui/button";
 import Link from "next/link";
@@ -13,16 +13,18 @@ export default async function UserCard({
   isOwner,
 }: {
   profileData?: ProfileData;
-  isOwner: boolean;
+  isOwner?: boolean;
 }) {
   console.log(profileData);
+
+  const icons = [Github, Instagram, Linkedin, Twitter, Plus];
 
   return (
     <div className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
       <div className="size-48">
         <img
           src={
-            (await getDownloadURLFromPath(profileData?.imagePath)) || "/me.webp"
+            (await getDownloadURLFromPath(profileData?.imagePath)) || "https://github.com/hosanabarcelos.png"
           }
           alt="Hosana Barcelos"
           className="rounded-full object-cover w-full h-full"
@@ -36,7 +38,7 @@ export default async function UserCard({
           {isOwner && <EditUserCard profileData={profileData} />}
         </div>
         <p className="opacity-40">
-          {profileData?.description || "Transformo ideias em soluções lucrativas."}
+          {profileData?.description || "Transformo ideias em soluções lucrativas!"}
         </p>
       </div>
       <div className="flex flex-col gap-2 w-full">
@@ -79,7 +81,15 @@ export default async function UserCard({
               <Twitter />
             </Link>
           )}
-
+          {!profileData &&
+            icons.map((Icon, index) => (
+              <button
+                key={index}
+                className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"
+              >
+                <Icon />
+              </button>
+            ))}
           {isOwner && (
             <EditSocialLinks socialMedias={profileData?.socialMedias} />
           )}
@@ -87,23 +97,39 @@ export default async function UserCard({
       </div>
       <div className="flex flex-col gap-3 w-full min-h-[172px]">
         <div className="w-full flex flex-col items-center gap-3">
-        {profileData?.link1?.title && profileData?.link1?.url && (
-          <Link href={formatUrl(profileData.link1.url)} target="_blank" className="w-full">
-            <Button className="w-full">{profileData.link1.title}</Button>
-          </Link>
-        )}
-        {profileData?.link2?.title && profileData?.link2?.url && (
-          <Link href={formatUrl(profileData.link2.url)} target="_blank" className="w-full">
-            <Button className="w-full">{profileData.link2.title}</Button>
-          </Link>
-        )}
-        {profileData?.link3?.title && profileData?.link3?.url && (
-          <Link href={formatUrl(profileData.link3.url)} target="_blank" className="w-full">
-            <Button className="w-full">{profileData.link3.title}</Button>
-          </Link>
-        )}
-
-        {isOwner && <AddCustomLink />}
+          {profileData?.link1 && (
+            <Link
+              href={formatUrl(profileData?.link1.url)}
+              target="_blank"
+              className="w-full"
+            >
+              <Button className="w-full">{profileData.link1.title}</Button>
+            </Link>
+          )}
+          {profileData?.link2 && (
+            <Link
+              href={formatUrl(profileData?.link2.url)}
+              target="_blank"
+              className="w-full"
+            >
+              <Button className="w-full">{profileData.link2.title}</Button>
+            </Link>
+          )}
+          {profileData?.link3 && (
+            <Link
+              href={formatUrl(profileData?.link3.url)}
+              target="_blank"
+              className="w-full"
+            >
+              <Button className="w-full">{profileData.link3.title}</Button>
+            </Link>
+          )}
+          {!profileData && (
+            <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]">
+              <Plus />
+            </button>
+          )}
+          {isOwner && <AddCustomLink />}
         </div>
       </div>
     </div>
