e3d1732999149716124fc2527d28698ce447c678
Hosana <hosanabarceloscandido@gmail.com>
2025-02-07 01:15:41 -0300

    feat: add lógica de total-visits para pagina principal

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -1,15 +1,16 @@
+import ProjectCard from "@/app/components/commons/project-card";
+import TotalVisits from "@/app/components/commons/total-visits";
+import UserCard from "@/app/components/commons/user-card/user-card";
 import { auth } from "@/app/lib/auth";
 import {
   getProfileData,
   getProfileProjects,
 } from "@/app/server/get-profile-data";
-import { notFound } from "next/navigation";
-import { getDownloadURLFromPath } from "@/app/lib/firebase";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 import NewProject from "./new-project";
-import ProjectCard from "@/app/components/commons/project-card";
-import TotalVisits from "@/app/components/commons/total-visits";
-import UserCard from "@/app/components/commons/user-card/user-card";
+import { getDownloadURLFromPath } from "@/app/lib/firebase";
+import { increaseProfileVisits } from "@/app/actions/increase-profile-visits";
 
 export default async function ProfilePage({
   params,
@@ -22,17 +23,15 @@ export default async function ProfilePage({
 
   if (!profileData) return notFound();
 
-  // TODO: get projects
-
   const projects = await getProfileProjects(profileId);
 
   const session = await auth();
 
   const isOwner = profileData.userId === session?.user?.id;
 
-  // TODO: Adicionar page view
-
-  // Se o usuario não estiver mais no trial, nao deixar ver o projeto. Redirecionar para upgrade
+  if (!isOwner) {
+    await increaseProfileVisits(profileId);
+  }
 
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
@@ -45,7 +44,7 @@ export default async function ProfilePage({
         </Link>
       </div>
       <div className="w-1/2 flex justify-center h-min">
-      <UserCard profileData={profileData} isOwner={isOwner}/>
+        <UserCard profileData={profileData} isOwner={isOwner} />
       </div>
       <div className="w-full flex justify-center content-start gap-4 flex-wrap overflow-y-auto">
         {projects.map(async (project) => (
@@ -58,9 +57,11 @@ export default async function ProfilePage({
         ))}
         {isOwner && <NewProject profileId={profileId} />}
       </div>
-      <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
-        <TotalVisits />
-      </div>
+      {isOwner && (
+        <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
+          <TotalVisits totalVisits={profileData.totalVisits} />
+        </div>
+      )}
     </div>
   );
 }
