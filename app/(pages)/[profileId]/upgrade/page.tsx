f60d8565c6fb8ae6a198aba27129ec9e058600be
Hosana <hosanabarceloscandido@gmail.com>
2025-02-09 15:20:11 -0300

    feat: importa e faz o uso de componente de botões de plano

--- app/(pages)/[profileId]/upgrade/page.tsx
+++ app/(pages)/[profileId]/upgrade/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/app/components/landing-page/header";
-import Button from "@/app/components/ui/button";
+import PlanButtons from "./plan-buttons";
 
 export default async function UpgradePage() {
   return (
@@ -7,8 +7,7 @@ export default async function UpgradePage() {
       <Header />
       <h2 className="text-2xl font-bold">Escolha o plano</h2>
       <div className="flex gap-4">
-        <Button>R$ 9,90 / mês</Button>
-        <Button>R$ 99,90 Vitalício</Button>
+        <PlanButtons />
       </div>
     </div>
   );
